855bb5003ce923c47b409b67a680ea59f1c39e0e
wulechuan <wulechuan@live.com>
2018-05-27 00:42:15 +0800

    .minttyrc for windows updated; scp quite

--- senario-specific-configurations/linux-in-vmware-wulechuan/after-making-this-senario.sh
+++ senario-specific-configurations/linux-in-vmware-wulechuan/after-making-this-senario.sh
@@ -1,2 +1,2 @@
-scp -r "$___here/dist/linux-in-vmware-wulechuan/bash-scripts" wulechuan@192.168.236.131:~
-scp "$___here/dist/linux-in-vmware-wulechuan/.bashrc" wulechuan@192.168.236.131:~
+scp -rq "$___here/dist/linux-in-vmware-wulechuan/bash-scripts" wulechuan@192.168.236.131:~
+scp -q "$___here/dist/linux-in-vmware-wulechuan/.bashrc" wulechuan@192.168.236.131:~
