bc08428f074eec60fe5f806689864c0f30a0cfbb
wulechuan <wulechuan@live.com>
2018-05-27 01:56:35 +0800

    single scp session

--- senario-specific-configurations/linux-in-vmware-wulechuan/after-making-this-senario.sh
+++ senario-specific-configurations/linux-in-vmware-wulechuan/after-making-this-senario.sh
@@ -1,2 +1,4 @@
-scp -rq "$___here/dist/linux-in-vmware-wulechuan/bash-scripts" wulechuan@192.168.236.131:~
-scp -q "$___here/dist/linux-in-vmware-wulechuan/.bashrc" wulechuan@192.168.236.131:~
+scp -rq \
+    "$___here/dist/linux-in-vmware-wulechuan/bash-scripts" \
+    "$___here/dist/linux-in-vmware-wulechuan/.bashrc" \
+    wulechuan@192.168.236.131:~
