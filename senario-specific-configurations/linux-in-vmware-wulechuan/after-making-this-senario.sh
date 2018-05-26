d6ce63a6598a70e570678060bec17f126544649f
wulechuan <wulechuan@live.com>
2018-05-27 02:47:27 +0800

    simplified scp

--- senario-specific-configurations/linux-in-vmware-wulechuan/after-making-this-senario.sh
+++ senario-specific-configurations/linux-in-vmware-wulechuan/after-making-this-senario.sh
@@ -1,11 +1,12 @@
-___entry1ToSCopy="$___here/dist/linux-in-vmware-wulechuan/bash-scripts"
-___entry2ToSCopy="$___here/dist/linux-in-vmware-wulechuan/.bashrc"
-
 echo ''
 echo -e "  \e[33mS-Copying files to linux in VM\e[0;0m"
-echo -en '  \e[31m'
-scp -rq "$___entry1ToSCopy" "$___entry2ToSCopy" wulechuan@192.168.236.131:~
-echo -e '  \e[0;0m'
 
-unset ___entry1ToSCopy
-unset ___entry2ToSCopy
+
+echo -en '  \e[31m'
+scp -rq \
+    "$___here/dist/linux-in-vmware-wulechuan/bash-scripts"\
+    "$___here/dist/linux-in-vmware-wulechuan/.bashrc"\
+    wulechuan@192.168.236.131:~
+
+
+echo -e '  \e[0;0m'
