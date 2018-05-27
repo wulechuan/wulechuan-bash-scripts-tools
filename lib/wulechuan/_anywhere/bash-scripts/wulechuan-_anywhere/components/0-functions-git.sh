a6b90235dd85809dd2b25e1ec27e64cac0bf7f0f
wulechuan <wulechuan@live.com>
2018-05-27 19:52:08 +0800

    git setup editor enhanced

--- lib/wulechuan/_anywhere/bash-scripts/wulechuan-_anywhere/components/0-functions-git.sh
+++ lib/wulechuan/_anywhere/bash-scripts/wulechuan-_anywhere/components/0-functions-git.sh
@@ -27,17 +27,23 @@ function git-try-to-set-editor-as-ms-vscode {
     local succeeded=0
 
     local codeOnLinux=/usr/bin/code
-    local codeOnWindows="c:/Program\ Files/Microsoft\ VS\ Code/Code.exe"
+    local codeOnWindows="$(WinDrive c)/Program Files/Microsoft VS Code/Code.exe"
 
     if [ -f $codeOnLinux ]; then
+
         git config --global core.editor $codeOnLinux
         succeeded=1
+
     elif [ -f "$codeOnWindows" ]; then
+
         git config --global core.editor "$codeOnWindows"
         succeeded=1
+    
     fi
 
     if [ $succeeded -eq 1 ]; then
         echo 'GIT: editor set to Microsoft Visual Studio Code'
+    else
+        echo -e "GIT:\e[31m Failed to set editor to Microsoft Visual Studio Code\e[0;0m"
     fi
 }
