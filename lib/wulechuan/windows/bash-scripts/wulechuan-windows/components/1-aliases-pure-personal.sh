7feb6ed6112c09aa1eaf11cd25fbeb923e5d7a74
wulechuan <wulechuan@live.com>
2018-05-27 19:54:09 +0800

    pure personal updated

--- lib/wulechuan/windows/bash-scripts/wulechuan-windows/components/1-aliases-pure-personal.sh
+++ lib/wulechuan/windows/bash-scripts/wulechuan-windows/components/1-aliases-pure-personal.sh
@@ -1,18 +1,7 @@
-# ------------------------------------------------
-#     wulechuan - aliases
-# ------------------------------------------------
-
-# 快捷命令
 alias sublime="$(WinDrive c)/Program\ Files/Sublime\ Text\ 3/sublime_text.exe"
 
-alias git-setup-all="git-setup-user-account &&
-    git-add-aliases-to-config &&
-    git-enable-colors &&
-    git-set-editor-as-ms-vscode
-"
 
-
-alias cddesktop="  cd d:/Users/wulechuan/Desktop"
+alias cddesktop="cd $(WinDrive d)/Users/wulechuan/Desktop"
 alias cdw1="cd $(WinDrive d)/projects/wulechuan/@wulechuan/"
 alias cdw2="cd $(WinDrive d)/projects/wulechuan/sundries/"
 alias cdw3="cd $(WinDrive d)/projects/wulechuan/once-served/"
