594d3fbd1fdec8632ac2b9b58f091d59c95aa855
wulechuan <wulechuan@live.com>
2018-05-28 14:45:19 +0800

    envVar used

--- lib/wulechuan/_anywhere/bash-scripts/wulechuan-_anywhere/init.sh
+++ lib/wulechuan/_anywhere/bash-scripts/wulechuan-_anywhere/init.sh
@@ -7,17 +7,17 @@ if [ "$USERNAME" = "lechuan.wu" ]; then
 fi
 
 ___shouldUse16ColorsPrompt=0
-if [ `uname`=="Linux" ] || [ `uname -o`=="Cygwin" ] || [ `uname -o`=="Msys" ]; then
+if [ envIsLinux = 1 ] || [ envIsCygwin = 1 ] || [ envIsGitBash = 1 ]; then
     ___shouldUse16ColorsPrompt=0
 else
     ___tputColors=`tput colors`
-    if [ $___tputColors -eq 8 ]; then
+    if [ $___tputColors = 8 ]; then
         ___shouldUse16ColorsPrompt=1
     fi
     unset ___tputColors
 fi
 
-if [ $___shouldUse16ColorsPrompt -eq 1 ]; then
+if [ $___shouldUse16ColorsPrompt = 1 ]; then
     export PROMPT_COMMAND='_customize_prompt_with_git_branch_info_in_16_colors_';
 else
     export PROMPT_COMMAND='_customize_prompt_with_git_branch_info_in_256_colors_'
