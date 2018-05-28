d9ab107433bcdfa4993e60c1f0e1cd43078ab4e7
wulechuan <wulechuan@live.com>
2018-05-28 15:50:42 +0800

    rename filled-template files

--- building-tools/make.sh
+++ building-tools/make.sh
@@ -1,3 +1,4 @@
 source "$___here/clean" # clean existing distribution if any
-source "$___here/building-tools/_make-packages.sh"
+source "$___here/building-tools/build-packages.sh"
+source "$___here/building-tools/after-building-packages.sh"
 source "$___here/senario-specific-configurations/after-making-all-senarios.sh"
