e2e0db1a12ebf0d99f00b53b72a38e770f58ecf8
wulechuan <wulechuan@live.com>
2018-05-30 14:23:01 +0800

    fixed clean issue

--- building-tools/make.sh
+++ building-tools/make.sh
@@ -1,5 +1,9 @@
+___shouldPreventRMFromAsyncDeletingNewlyCreatedFilledTemplates=1
+
 source "$___here/clean" # clean existing distribution if any
 source "$___here/building-tools/before-building-packages.sh"
 source "$___here/building-tools/build-packages.sh"
 source "$___here/building-tools/after-building-packages.sh"
 source "$___here/senario-specific-configurations/after-making-all-senarios.sh"
+
+unset ___shouldPreventRMFromAsyncDeletingNewlyCreatedFilledTemplates
