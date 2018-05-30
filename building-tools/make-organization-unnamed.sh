f0d07c6d8d6570892d42e6aa95f72cf5e0586980
wulechuan <wulechuan@live.com>
2018-05-30 14:34:51 +0800

    fixed a bug

--- building-tools/make-organization-unnamed.sh
+++ building-tools/make-organization-unnamed.sh
@@ -12,7 +12,7 @@ if [ $___netisInputReturnCode -eq 0 ]; then
     source "$___here/building-tools/after-building-packages.sh"
     source "$___here/senario-specific-configurations/after-making-all-senarios.sh"
 
-    unset ___shouldPreventRMFromAsyncDeletingNewlyCreatedFilledTemplates=1
+    unset ___shouldPreventRMFromAsyncDeletingNewlyCreatedFilledTemplates
 fi
 
 # unset ___netisInputReturnCode # 此时还不能删除改变量
