73e370dad94b7dd814bd9cec91f15bc0a028bce9
wulechuan <wulechuan@live.com>
2018-05-30 15:39:51 +0800

    read with timeout

--- senario-specific-configurations/fill-variable-values-for-unnamed-organization-env.sh
+++ senario-specific-configurations/fill-variable-values-for-unnamed-organization-env.sh
@@ -35,7 +35,7 @@ function ___build_variables_from_tempaltes_for_unnamedOrg_env {
             echo -n "Press any key to continue "
 		fi
 
-        read
+        read -n 1 -t 10
 
         echo
     else
