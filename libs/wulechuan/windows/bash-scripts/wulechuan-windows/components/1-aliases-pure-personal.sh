0370a0296823db2326e94cd965e1066425a56847
wulechuan <wulechuan@live.com>
2018-06-01 16:18:35 +0100

    cdbs added

--- libs/wulechuan/windows/bash-scripts/wulechuan-windows/components/1-aliases-pure-personal.sh
+++ libs/wulechuan/windows/bash-scripts/wulechuan-windows/components/1-aliases-pure-personal.sh
@@ -5,6 +5,7 @@ alias cddesktop="cd $(WinDrive d)/Users/wulechuan/Desktop"
 alias cdw1="cd $(WinDrive d)/projects/wulechuan/@wulechuan/"
 alias cdw2="cd $(WinDrive d)/projects/wulechuan/sundries/"
 alias cdw3="cd $(WinDrive d)/projects/wulechuan/once-served/"
+alias cdbs="cd $(WinDrive d)/projects/wulechuan/non-web/wulechuan-bash-scripts-tools/"
 
 
 alias sshvmlinux="ssh wulechuan@192.168.236.131"
