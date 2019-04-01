2d280fa6db85f5588b37bc69d5aeb28ea68c804e
wulechuan <wulechuan@live.com>
2019-04-01 15:41:23 +0800

    ssh copy id 改善

--- source/components/optional/_anyone-_anywhere/wlc-bash-tools/bash-auto-load-on-start/functions/ssh.sh
+++ source/components/optional/_anyone-_anywhere/wlc-bash-tools/bash-auto-load-on-start/functions/ssh.sh
@@ -5,15 +5,15 @@ function wlc--ssh_keygen {
     function print-help {
         echo
         if [ "$copywritingLanguage" = "zh_CN" ]; then
-            colorful -n "欲创建一个ssh密钥，请指明完整的用户ID。用户ID应依次包含用户名和域名两部分，以@符号连接。"    textRed
-            colorful -- "    范例： "
+            colorful -n "欲创建一个ssh密钥，请指明完整的用户ID。用户ID应依次包含用户名和域名两部分，以 @ 符号连接。"    textRed
+            colorful -n "范例： "
         else
             colorful -n "Please provide an ID."    textRed
-            colorful -- "    Example: "
+            colorful -n "Example: "
         fi
 
-        colorful -- 'sshkg '                textGreen
-        colorful -- '"'                     textBrightBlack
+        colorful -- '    wlc--ssh_keygen'   textGreen
+        colorful -- '    "'                 textBrightBlack
         colorful -- "wulechuan@live.com"    textCyan
         colorful -n '"'                     textBrightBlack
         echo
@@ -24,11 +24,16 @@ function wlc--ssh_keygen {
     local newIdHumanReadableName=$2
 
     if [ -z "$newId" ]; then
+        console.error    "Invalid \$1. The provided value was \"\e[33m${newId}\e[31m\"."
         print-help
         return 1
     fi
 
     if [ -z "$newIdHumanReadableName" ]; then
+        colorful -- "\$2, aka the human readable name is not provided. "    textYellow
+        colorful -- "The default value \""    textYellow
+        colorful -- "$newId"                  textMagenta
+        colorful -n "\" is used."             textYellow
         newIdHumanReadableName="$newId"
     fi
 
@@ -76,21 +81,22 @@ function wlc--ssh_keygen {
 
     colorful -n $VE_line_60
     echo
-    
 
 
 
-    wlc--ssh_copy_id    "$backupKeyFilePath"    $newId
+
+    wlc--ssh_copy_id    $newId    "$backupKeyFilePath"
     echo
 }
 
 
 function wlc--ssh_copy_id {
     function print-examples {
-        local exampleFile1="~/.ssh/backup/wulechuan@live.com.sshkey"
-        local exampleFile2="~/.ssh/id_rsa                          "
+        # local exampleFile1="~/.ssh/id_rsa                          "
+        local exampleFile2="~/.ssh/backup/wulechuan@live.com.sshkey"
         local exampleHost="wulechuan@github.com"
 
+        echo
 
         if [ "$copywritingLanguage" = "zh_CN" ]; then
             colorful -n "范例：${example1}"
@@ -99,20 +105,24 @@ function wlc--ssh_copy_id {
         fi
 
         colorful -- "    wlc--ssh_copy_id"    textGreen
-        colorful -- "    $exampleFile1"       textBlue
-        colorful -n "    $exampleHost"        textMagenta
+        colorful -- "    $exampleHost"        textMagenta
+        # colorful -- "    $exampleFile1"       textBlue
+        echo
 
         colorful -- "    wlc--ssh_copy_id"    textGreen
+        colorful -- "    $exampleHost"        textMagenta
         colorful -- "    $exampleFile2"       textBlue
-        colorful -n "    $exampleHost"        textMagenta
+        echo
     }
 
     function print-help-1 {
         echo
         if [ "$copywritingLanguage" = "zh_CN" ]; then
-            colorful -n '请指明ssh密钥文件。'    textRed
+            console.error    "函数 wlc--ssh_copy_id: 无效的【参数1】。得到的值为“\e[33m${remoteHost}\e[31m”"
+            colorful -n '请指明完整的用户ID。用户ID应依次包含用户名和域名两部分，以 @ 符号连接。'    textRed
         else
-            colorful -n 'Please provide an ssh key file.'    textRed
+            console.error    "function wlc--ssh_copy_id: Invalid \$1. The provided value was \"\e[33m${remoteHost}\e[31m\"."
+            colorful -n 'Please provide an ID.'    textRed
         fi
         print-examples
         echo
@@ -121,11 +131,54 @@ function wlc--ssh_copy_id {
     function print-help-2 {
         echo
         if [ "$copywritingLanguage" = "zh_CN" ]; then
-            colorful -n '请指明完整的用户ID。用户ID应依次包含用户名和域名两部分，以 @ 符号连接。'    textRed
+            console.error    "函数 wlc--ssh_copy_id："
+
+            colorful -- "    【参数2】得到的值为“"    textRed
+            colorful -- "${localSSHKeyFile}"       textYellow
+            colorful -n "”。该文件不存在。"           textRed
         else
-            colorful -n 'Please provide an ID.'    textRed
+            console.error    "function wlc--ssh_copy_id:"
+
+            colorful -- "    \$2 was \""    textRed
+            colorful -- "${localSSHKeyFile}"    textYellow
+            colorful -n "\", which is invalid."    textRed
         fi
-        print-examples
+        echo
+    }
+
+    function print-help-3 {
+        echo
+        if [ "$copywritingLanguage" = "zh_CN" ]; then
+            console.error    "函数 wlc--ssh_copy_id："
+
+            colorful -- "    默认的 SSH 密钥文件“"    textRed
+            colorful -- "${localSSHKeyFile}"       textYellow
+            colorful -n "”亦不存在。"                textRed
+        else
+            console.error    "function wlc--ssh_copy_id:"
+
+            colorful -- "    The default SSH key file \""    textRed
+            colorful -- "${localSSHKeyFile}"                 textYellow
+            colorful -n "\" does not exist either."          textRed
+        fi
+        echo
+    }
+
+    function print-tip-of-default-ssh-key-file-used {
+        echo
+        if [ "$copywritingLanguage" = "zh_CN" ]; then
+            colorful -n "函数 wlc--ssh_copy_id："    textYellow
+
+            colorful -- "    【参数2】未给出。故将采用默认的 SSH 密钥文件，即“"    textYellow
+            colorful -- "~/.ssh/id_rsa"    textMagenta
+            colorful -n "”。"    textYellow
+        else
+            colorful -n "function wlc--ssh_copy_id:"    textYellow
+
+            colorful -- "    \$2 was not provided. Thus the default \""    textYellow
+            colorful -- "~/.ssh/id_rsa"    textMagenta
+            colorful -- "\" will be used instead."    textYellow
+        fi
         echo
     }
 
@@ -134,18 +187,27 @@ function wlc--ssh_copy_id {
 
 
 
-    local sshKeyFile=$1
-    local remoteHost=$2
+    local remoteHost="$1"
+    local localSSHKeyFile="$2"
 
-    if [ -z "$sshKeyFile" ]; then
+    if [ -z "$remoteHost" ]; then
         print-help-1
         return 1
     fi
 
-    if [ -z "$remoteHost" ]; then
+    if [ -z "$localSSHKeyFile" ]; then
+        print-tip-of-default-ssh-key-file-used
+        localSSHKeyFile=~/.ssh/id_rsa
+
+        if [ ! -f $localSSHKeyFile ]; then
+            print-help-3
+            return 3
+        fi
+    elif [ ! -f "$localSSHKeyFile" ]; then
         print-help-2
         return 2
     fi
+
 
 
 
@@ -158,5 +220,5 @@ function wlc--ssh_copy_id {
     else
         print-header "Copying SSH pub key to \"${remoteHost}\"..."
     fi
-    ssh-copy-id -i $sshKeyFile ${remoteHost}
+    ssh-copy-id    -i "$localSSHKeyFile"    "$remoteHost"
 }
